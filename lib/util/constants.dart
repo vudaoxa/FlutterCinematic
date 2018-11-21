808b80a7403ca11e9266ed77a79e8c7be38f8b76
vunq <vudaoxa@gmail.com>
2018-11-21 17:06:00 +0700

    [CFG] Flutter dev initial

--- lib/util/constants.dart
+++ lib/util/constants.dart
@@ -1,2 +1,2 @@
 // TODO: To run the app you need an api-key. Get one here: https://developers.themoviedb.org/3/getting-started/introduction
-const String API_KEY = <your-api-key>;
+const String API_KEY = "39db621746dc609930d24faf9d54da50";
